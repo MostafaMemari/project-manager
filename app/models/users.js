bd66100aff6ef193cf9000dc407dfb2e3485793c
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-09-25 19:20:53 +0330

    develop team , project and users Models

---
+++ app/models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    first_name: { type: String },
+    last_name: { type: String },
+    username: { type: String, required: true, unique: true },
+    mobile: { type: String, required: true, unique: true },
+    roles: { type: String, default: ["USER"] },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    skills: { type: String, default: [] },
+    teams: { type: String, default: [] },
+  },
+  {
+    timestamps: true,
+  }
+);
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = { userModel };
