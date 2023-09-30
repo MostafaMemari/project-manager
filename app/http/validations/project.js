ffa881288423d42a045cb55ff4acf4c8f18cbfed
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-09-30 20:13:27 +0330

    add tag project

--- app/http/validations/project.js
+++ app/http/validations/project.js
@@ -3,6 +3,7 @@ const { body } = require("express-validator");
 function createProjectValidator() {
   return [
     body("title").notEmpty().withMessage("عنوان پروژه نمیتواند خالی باشد"),
+    body("tags").isArray({ min: 0, max: 10 }).withMessage("حداکثر استفاده از هشتک ها 10 عدد می باشد "),
     body("text").notEmpty().isLength({ min: 20 }).withMessage("توضیحات نویسه نمیتواند خالی باشد و حداقل باید 25 کاراکتر باشد"),
   ];
 }
