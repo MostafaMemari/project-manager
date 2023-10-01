878de239b7a3ce4b7906cca596deb8536fcb69e5
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-10-01 12:24:24 +0330

    done route update project

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -68,6 +68,29 @@ class ProjectController {
   }
   async updateProject(req, res, next) {
     try {
+      const owner = req.user._id;
+      const projectID = req.params.id;
+      const project = await this.findProject(projectID, owner);
+
+      const data = { ...req.body };
+
+      Object.entries(data).forEach(([key, value]) => {
+        if (!["title", "text", "tags"].includes(key)) delete data[key];
+        if (["", " ", 0, null, undefined, NaN].includes(value)) delete data[key];
+        if (key == "tags" && data["tags"].constructor === Array) {
+          data["tags"] = data["tags"].filter((val) => {
+            if (!["", " ", 0, null, undefined, NaN].includes(val)) return val;
+          });
+          if (data["tags"].length == 0) delete data["tags"];
+        }
+      });
+      const updateResult = await projectModel.updateOne({ _id: projectID }, { $set: data });
+      if (updateResult.modifiedCount == 0) throw { status: 400, message: "بروزرسانی انجام نشد" };
+      return res.status(200).json({
+        status: 200,
+        success: true,
+        message: "بروزرسانی با موفقیت انجام شد",
+      });
     } catch (error) {
       next(error);
     }
